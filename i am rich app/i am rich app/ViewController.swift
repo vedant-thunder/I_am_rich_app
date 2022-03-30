0559af253df6274677687297d5a0264dc1038afd
vedant-thunder <89499026+vedant-thunder@users.noreply.github.com>
2022-03-30 22:01:14 +0530

    Add files via upload

---
+++ i am rich app/i am rich app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich app
+//
+//  Created by Mac on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
